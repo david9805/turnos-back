8ba2a01afa778042cf4eac4e7f7b640f2f45c302
david9805 <100810560+david9805@users.noreply.github.com>
2024-09-23 20:08:52 -0500

    Fix

--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -1,8 +1,34 @@
+import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class SmsService {
-    constructor(){
-        
+    constructor(private http:HttpService){
+
+    }
+
+    async sendSms(phone:string,message:string,nameClient:string){
+        const data = {
+            "idSmsCategory": 1,
+            "name": "SMS202409",
+            "template": "Prueba",
+            "dateNow": 1,
+            "type": "lote",
+            "track": 0,
+            "sendPush": 0,
+            "api": 1,
+            "notification": 0,
+            "email": "davids59@hotmail.com",
+            "rne": 0,
+            "receiver": [
+                  {
+                      "%%NOMBRE%%": nameClient,
+                      "indicative": 57,
+                      "phone": 3212875783          
+                      /*"message": "Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno  de las industrias desde el año 1500 cuando un impresor (N. del T. persona que se dedica a la imprenta) https://www.google.com/"*/
+                  }
+              ]
+        }
+        this.http.post();
     }
 }
